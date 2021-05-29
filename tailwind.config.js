7c151c39a14673756ec3b66f11fd104878ed7159
Jonathan Nascimento <nascimento.jonathan@live.com>
2021-05-29 08:42:14 -0300

    ajustes iniciais.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,4 +11,13 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  // config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
 }
